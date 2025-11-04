9f81c1a4df8824d8ea97ef3579ffac35f032dc04
barganakukuhraditya <barganakukuh@gmail.com>
2025-11-04 21:13:30 +0700

    komen

--- routes/variables.go
+++ routes/variables.go
@@ -2,6 +2,7 @@ package routes
 
 import "backendmailingroom/repository"
 
+// Declare repository variables
 var (
 	UserRepository          repository.UserRepository
 	SubdirektoratRepository repository.SubdirektoratRepository
